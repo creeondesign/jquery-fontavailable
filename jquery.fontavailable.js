0155295ec5cceb3c5c0d9e18af34b515770fa5a2
howardr <howardr@82768b32-0208-11de-9f19-3372612f5e33>
2009-02-24 00:29:56 +0000

    initial check in

---
+++ jquery.fontavailable.js
@@ -0,0 +1,26 @@
+/* Font Available jQuery Plugin, v1.0
+ *
+ * Copyright (c) 2009, Howard Rauscher
+ * Licensed under the MIT License
+ */
+ 
+(function($) {
+    $.fontAvailable = function(fontName) {
+        var element, width;
+        
+        // prepare element, and append to DOM
+        element = $(document.createElement('span'))
+            .css('visibility', 'hidden')
+            .html('abcdefghijklmnopqrstuvwxyz')
+            .appendTo(document.body);
+        
+        // get the width of element after applying a fake font
+        width = element
+            .css('font-family', '__FAKEFONT__')
+            .width();
+        
+        element.css('font-family', fontName);
+        
+        return (width !== element.width());
+    }
+})(jQuery);
