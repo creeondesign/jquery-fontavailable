e8fbf7e79ea3e5ae048e7330b29f2a43fadf99ee
howardr <howardr@82768b32-0208-11de-9f19-3372612f5e33>
2009-04-30 15:46:57 +0000

    testing element node height in addition to width.  also, positioning node out of viewport

--- trunk/jquery.fontavailable.js
+++ trunk/jquery.fontavailable.js
@@ -1,4 +1,4 @@
-/* fontAvailable jQuery Plugin, v1.0
+/* fontAvailable jQuery Plugin, v1.1
  *
  * Copyright (c) 2009, Howard Rauscher
  * Licensed under the MIT License
@@ -8,20 +8,28 @@
 	var element;
 	
     $.fontAvailable = function(fontName) {
-        var width;    
-        // prepare element, and append to DOM
-        !element && ( element = $( document.createElement( 'span' ))
-            .css('visibility', 'hidden')
-            .html('abcdefghijklmnopqrstuvwxyz')
-            .appendTo( document.body ));
+        var width, height;
         
-        // get the width of element after applying a fake font
+        // prepare element, and append to DOM
+        if(!element) {
+            element = $( document.createElement( 'span' ))
+                .css( 'visibility', 'hidden' )
+                .css( 'position', 'absolute' )
+                .css( 'top', '-10000px' )
+                .css( 'left', '-10000px' )
+                .html( 'abcdefghijklmnopqrstuvwxyz' )
+                .appendTo( document.body );
+        }
+        
+        // get the width/height of element after applying a fake font
         width = element
             .css('font-family', '__FAKEFONT__')
             .width();
+        height = element.height();
         
+        // set test font
         element.css('font-family', fontName);
         
-        return width !== element.width();
+        return width !== element.width() || height !== element.height();
     }
 })(jQuery);
